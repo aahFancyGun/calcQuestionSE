900707ae9a348bae0241a74d7a1bb5ef452152e5
aahFancyGun <yangfanchau2008@gmail.com>
2025-05-06 13:33:18 -0500

    serpapi working in python file, we should start distributing roles soon

--- server.py
+++ server.py
@@ -1,2 +1,13 @@
 from flask import Flask, render_template, send_file
 import subprocess
+
+from serpapi import GoogleSearch
+
+params = {
+  "q": "Coffee",
+  "api_key": "a2a1474b2e6ba99d36ae610c49bb60d13f3cc56025adf403882509320f517bcd"
+}
+
+search = GoogleSearch(params)
+results = search.get_dict()
+print(results)
