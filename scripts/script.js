83a16f1d2242de531e9736f6c4d3b876c036f3c7
1aidan2b2 <1aidan2b2@gmail.com>
2025-05-20 13:03:13 -0500

    asdfsadf
    cheese

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,9 @@
 window.onload = function() {
 document.getElementById("loading").style.display = "none";
+document.getElementById("Image").src="../assets/Calcuintro.gif";
+
 };
 
-  
 function showLoading() {
     document.getElementById("loading").style.display = "block";
 }
